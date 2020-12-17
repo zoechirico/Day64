67d6d1ba8e191cdfb264284aa05ec6e8a1fb38f7
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2020-12-17 08:52:29 -0500

    Initial Commit

---
+++ Day64/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day64
+//
+//  Created by Mike Chirico on 12/17/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day64/Day64App.swift
@@ -0,0 +1,17 @@
+//
+//  Day64App.swift
+//  Day64
+//
+//  Created by Mike Chirico on 12/17/20.
+//
+
+import SwiftUI
+
+@main
+struct Day64App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
